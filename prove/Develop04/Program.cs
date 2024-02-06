08ec3497e16ce85d156285d4977d59ebce8f5fa3
Gabriel09J <134187761+Gabriel09J@users.noreply.github.com>
2024-02-05 21:43:28 -0700

    Update Program.cs
    Updated program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -283,13 +283,13 @@ private void PerformRandomReflection(int durationInSeconds)
 class ListeningActivity : BaseActivity
 {
     private string[] listeningPrompts =
-    {
-        “Who are people that you appreciate?”,
-        “What are personal strengths of yours?”,
-        “Who are people that you have helped this week?”,
-        “When have you felt the Holy Ghost this month?”,
-        “Who are some of your personal heroes?”
-    };
+{
+    "Who are people that you appreciate?",
+    "What are personal strengths of yours?",
+    "Who are people that you have helped this week?",
+    "When have you felt the Holy Ghost this month?",
+    "Who are some of your personal heroes?"
+};
 
     public ListeningActivity(string name, string description) : base(name, description) { }
 
